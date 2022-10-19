deb115b5ca1d17ce4b15d0501c6250a192ad2c91
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-10-19 05:17:32 +0200

    :dizzy:feat: first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Introduction
+
+- Pomodoro 
+
+Button start/pause : launch the timer
+Button reset : reset the timer
+
+- Can define repeat 
+- Can define time
+- Can define rest timer
+
+Keywords change Break or Work session => color theme change
+
+- Display break time + work time 
+- Switch display time when break or work
+
---
+++ script/index.js
@@ -0,0 +1,21 @@
+// console.log('Hello from space 🚀')
+
+const app = {
+    //~ VARIABLES
+
+    //* buttons
+    startBtn: document.querySelector('container__button--start'),
+    resetBtn: document.querySelector('container__button--reset'),
+
+    //~ INIT
+    init: () => { },
+    
+    //~ METHODS
+    handleListeners: () => { },
+    startTimer: () => { },
+    resetTimer: () => { },
+    breakAction: () => { },
+    handleBtnMore: () => { },
+    handleBtnLess: () => { },
+
+}
